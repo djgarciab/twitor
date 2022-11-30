47d674d568782282055d0bb694170f7d72b7a931
Diego Garcia <djgarciab@ibm.com>
2022-11-30 11:39:49 -0500

    modif de iphone x+

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 // imports
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE =    'static-v3';
-const DYNAMIC_CACHE =   'dynamic-v2';
+const STATIC_CACHE =    'static-v4';
+const DYNAMIC_CACHE =   'dynamic-v3';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 // LO QUE UNO CREA Y MODIFICA
@@ -46,6 +46,9 @@ self.addEventListener('activate', e => {
     const respuesta = caches.keys().then( keys => {
         keys.forEach( key => {
             if ( key !== STATIC_CACHE && key.includes('static') ) {
+                return caches.delete(key);
+            }
+            if ( key !== DYNAMIC_CACHE && key.includes('dynamic') ) {
                 return caches.delete(key);
             }
         });
